14c9aa2ea86ba34626026b6d87656a178e1c4761
Darkff <114341147+Kiruha22882@users.noreply.github.com>
2022-11-06 16:18:36 +0300

    znan7

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     $(".bxslider").bxSlider({ pager: false });
+    $(".bxslider_team").bxSlider({});
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 200,
+        itemMargin: 25,
+        touch: true
+    });
 });
+
+
